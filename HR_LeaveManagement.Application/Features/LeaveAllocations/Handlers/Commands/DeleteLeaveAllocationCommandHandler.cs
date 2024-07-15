be430c6161e94bd14d988bc0496cf5bbadcb878c
Akib <ali.haider@brainstation-23.com>
2024-07-15 15:16:14 +0600

    Update delete command for leave request, leave type, and leave allocation features

---
+++ HR_LeaveManagement.Application/Features/LeaveAllocations/Handlers/Commands/DeleteLeaveAllocationCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HR_LeaveManagement.Application.Features.LeaveAllocations.Requests.Commands;
+using HR_LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_LeaveManagement.Application.Features.LeaveAllocations.Handlers.Commands
+{
+    internal class DeleteLeaveAllocationCommandHandler : IRequestHandler<DeleteLeaveAllocationCommand>
+    {
+        private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+        private readonly IMapper _mapper;
+        public DeleteLeaveAllocationCommandHandler(ILeaveAllocationRepository leaveAllocationRepository, IMapper mapper)
+        {
+            _leaveAllocationRepository = leaveAllocationRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(DeleteLeaveAllocationCommand request, CancellationToken cancellationToken)
+        {
+            var dataFromDb = await _leaveAllocationRepository.GetAsync(request.Id);
+            await _leaveAllocationRepository.DeleteAsync(dataFromDb);   
+            return Unit.Value;
+        }
+    }
+}
---
+++ HR_LeaveManagement.Application/Features/LeaveRequests/Handler/Commands/DeleteLeaveRequestCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HR_LeaveManagement.Application.Features.LeaveRequests.Request.Commands;
+using HR_LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_LeaveManagement.Application.Features.LeaveRequests.Handler.Commands
+{
+    public class DeleteLeaveRequestCommandHandler : IRequestHandler<DeleteLeaveRequestCommand>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly IMapper _mapper;
+        public DeleteLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(DeleteLeaveRequestCommand request, CancellationToken cancellationToken)
+        {
+            var dataFromDb = await _leaveRequestRepository.GetAsync(request.Id);
+            await _leaveRequestRepository.DeleteAsync(dataFromDb);
+            return Unit.Value;
+        }
+    }
+}
